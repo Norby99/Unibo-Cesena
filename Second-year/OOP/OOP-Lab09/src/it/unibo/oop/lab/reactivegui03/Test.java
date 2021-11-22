94d0c4c644b77c399fdb1c64ceab9d0b296702a8
Norbert Gabos <dont.vip@gmail.com>
2021-11-22 14:22:06 +0100

    added new Class; issue: it doesn't stop

--- Second-year/OOP/OOP-Lab09/src/it/unibo/oop/lab/reactivegui03/Test.java
+++ Second-year/OOP/OOP-Lab09/src/it/unibo/oop/lab/reactivegui03/Test.java
@@ -28,6 +28,6 @@ public final class Test {
      * 
      */
     public static void main(final String... args) {
-        // new AnotherConcurrentGUI();
+        new AnotherConcurrentGUI();
     }
 }
