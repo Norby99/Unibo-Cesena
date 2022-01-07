097c7b25c24b28896573cf540032bb734bfec0c2
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-01-07 14:57:04 +0100

    finished mvcio

--- Second-year/OOP/OOP-Lab08/src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ Second-year/OOP/OOP-Lab08/src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,17 +1,19 @@
 package it.unibo.oop.lab.mvcio;
-
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
-/**
- * A very simple program using a graphical interface.
- * 
- */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("SimpleGUI");
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -36,27 +38,47 @@ public final class SimpleGUI {
     /**
      * builds a new {@link SimpleGUI}.
      */
-    public SimpleGUI() {
+    public SimpleGUI(final Controller ctrl) {
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
          * primary is selected.
-         * 
-         * In order to deal coherently with multimonitor setups, other
-         * facilities exist (see the Java documentation about this issue). It is
-         * MUCH better than manually specify the size of a window in pixel: it
-         * takes into account the current resolution.
          */
-        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();   // setting up the frame
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / 2, sh / 2);
-        /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
-         */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
+        
+        JPanel canvas = new JPanel();   // setting up the components
+        canvas.setLayout(new BorderLayout());
+        JTextArea textArea = new JTextArea();
+        JButton btnSave = new JButton("Save");
+        
+        canvas.add(textArea, BorderLayout.NORTH);   // adding the components to the canvas and frame
+        canvas.add(btnSave, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    ctrl.saveToFile(textArea.getText());
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        });
     }
-
+    
+    public void display() {
+        frame.setVisible(true);
+    }
+    
+    public static void main(final String... a) {
+        SimpleGUI myGUI = new SimpleGUI(new Controller());
+        myGUI.display();
+    }
 }
