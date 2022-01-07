0004123da0bd54e55df4cc5429113b94d0ca8d45
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-01-07 15:49:01 +0100

    first setup

--- Second-year/OOP/OOP-Lab08/src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ Second-year/OOP/OOP-Lab08/src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -5,10 +5,6 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-/**
- * A very simple program using a graphical interface.
- * 
- */
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
@@ -16,7 +12,7 @@ public final class SimpleGUI {
     /*
      * Once the Controller is done, implement this class in such a way that:
      * 
-     * 1) I has a main method that starts the graphical application
+     * 1) It has a main method that starts the graphical application
      * 
      * 2) In its constructor, sets up the whole view
      * 
@@ -39,27 +35,17 @@ public final class SimpleGUI {
      */
     public SimpleGUI() {
 
-        /*
-         * Make the frame half the resolution of the screen. This very method is
-         * enough for a single screen setup. In case of multiple monitors, the
-         * primary is selected.
-         * 
-         * In order to deal coherently with multimonitor setups, other
-         * facilities exist (see the Java documentation about this issue). It is
-         * MUCH better than manually specify the size of a window in pixel: it
-         * takes into account the current resolution.
-         */
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
         frame.setSize(sw / 2, sh / 2);
 
-        /*
-         * Instead of appearing at (0,0), upper left corner of the screen, this
-         * flag makes the OS window manager take care of the default positioning
-         * on screen. Results may vary, but it is generally the best choice.
-         */
         frame.setLocationByPlatform(true);
+    }
+    
+    public static void main() {
+        SimpleGUI myGUI = new SimpleGUI(new MyController());
+        myGUI.display();
     }
 
 }
