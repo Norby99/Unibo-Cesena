46dd8a29811878f6a6ed111cd546fc0e24724322
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-06-14 15:24:35 +0200

    order processing

--- Second-year/Reti/gateway.py
+++ Second-year/Reti/gateway.py
@@ -31,6 +31,12 @@ class Gateway():
     __is_client_connected: bool = False
 
     def __init__(self, gateway_port: int) -> None:
+        order = {
+            "drone_id": "drone_4",
+            "order_destination": "burundi"
+        }
+        if order["drone_id"] not in self.__drones.keys():
+            raise Exception("Wrong drone id")
         self._setup_gateway_client(gateway_port)
 
         self.__connect_client()
@@ -53,16 +59,64 @@ class Gateway():
         with self.__client["conn"]:
             self.send_drone_information()
 
-    def send_drone_information(self) -> None:
+        self.listen_for_orders()
+
+    def listen_for_orders(self) -> None:
+        """
+        Listen if there are orders to be shipped.
+        """
+        while True:
+            data = self.__client["conn"].recv(self.__buffer_size)
+            if not data:
+                break
+            order = eval(data.decode("utf-8"))
+            if not self._check_order(order):
+                #TODO: send error message to client
+                pass
+
+            print(f"Order: {order}")
+            self._process_order(order)
+
+    def send_drone_information(self) -> bool:
         """
         Send the drones id that are free to the client.
         """
         free_drones = [drone["id"] for drone in self.__drones.values() if drone['status'] == 'free']
         try:
             self.__client["conn"].send(str(free_drones).encode())
+            return True
         except:
             print(f"Client {self.__client['address']} disconnected!")
             self.__is_client_connected = False
+            return False
+
+    def _process_order(self, order: dict) -> None:
+        """
+        Process the order and send the drone to the order destination.
+        """
+        self.__drones[order["drone_id"]]["status"] = "shipping"
+        #TODO: send drone to order destination
+
+    def _check_order(self, order: dict) -> bool:
+        """
+        Check if the order is valid and if the drone is available.
+        """
+        # check if the order syntax is valid
+        if not all(keys in order for keys in ("drone_id", "order_destination")):
+            print("Wrong order syntax")
+            return False
+
+        # check if the drone exists
+        if order["drone_id"] not in self.__drones.keys():
+            print("The selected drone does not exist")
+            return False
+
+        # check if the drone is free
+        if self.__drones[order["drone_id"]]["status"] != "free":
+            print("The selected drone is not free")
+            return False
+        
+        return True
 
     def close_client_connection(self) -> None:
         """
