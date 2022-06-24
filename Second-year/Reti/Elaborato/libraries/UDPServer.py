2b75bb60a48b9cef1e949b027a17671316f96428
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-06-24 14:27:18 +0200

    created simple UDP server

---
+++ Second-year/Reti/Elaborato/libraries/UDPServer.py
@@ -0,0 +1,56 @@
+import socket
+from datetime import datetime
+
+class UDPServer():
+    ''' A simple UDP Server '''
+    
+    def __init__(self, host, port):
+
+        self.host = host    # Host address
+        self.port = port    # Host port
+        self.sock = None    # Socket
+
+    def printwt(self, msg):
+        '''
+        Print message with current date and time
+        '''
+        current_date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'[{current_date_time}] {msg}')
+
+    def configure_server(self):
+        '''
+        Configure the server
+        '''
+        self.printwt('Creating socket...')
+        self.printwt('Socket created')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        self.printwt(f'Binding server to {self.host}:{self.port}...')
+        self.printwt(f'Server binded to {self.host}:{self.port}')
+
+    def handle_request(self, data, client_address):
+        '''
+        Handle the client
+        '''
+        name = data.decode('utf-8')
+        self.printwt(f'[ REQUEST from {client_address} ]')
+        print('\n', name, '\n')
+        resp = self.get_phone_no(name)
+
+        self.printwt(f'[ RESPONSE to {client_address} ]')
+        self.sock.sendto(resp.encode('utf-8'), client_address)
+        print('\n', resp, '\n')
+
+    def wait_for_client(self):
+        ''' Wait for a client '''
+        try:
+            # receive message from a client
+            data, client_address = self.sock.recvfrom(1024)
+            # handle client's request
+            self.handle_request(data, client_address)
+        except OSError as err:
+            self.printwt(err)
+
+    def shutdown_server(self):
+        ''' Shutdown the UDP server '''
+        self.sock.close()
