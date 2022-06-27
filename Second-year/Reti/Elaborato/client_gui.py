8f6c475b508bb637a418385da155082187b2b75b
Norby99 <72309039+Norby99@users.noreply.github.com>
2022-06-27 12:37:25 +0200

    minor improvements

--- Second-year/Reti/Elaborato/client_gui.py
+++ Second-year/Reti/Elaborato/client_gui.py
@@ -2,18 +2,25 @@ import tkinter as tk
 import json
 from client import Client
 
-class Client_GUI(tk.Frame):
+class Client_GUI():
     """
     Class that implements a GUI for the Client class
     """
-    def __init__(self, ip, port, master=None):
-        super().__init__(master)
+
+    __time_after: int = 100
+
+    def __init__(self, ip, port):
+        # create the main window
+        self.root = tk.Tk()
+        self.root.title("Client")
+        self.root.geometry("300x300")
+        self.root.resizable(False, False)
 
         self.client = Client(ip, port)
         self.create_widgets()
         
-        while True:
-            self.client.main_tick()
+        self.root.after(self.__time_after, self.task)
+        self.root.mainloop()
         
         self.client.close_connection()
         
@@ -21,28 +28,28 @@ class Client_GUI(tk.Frame):
     # create widgets for drone_id and shipment
     def create_widgets(self):
         # show available drones
-        self.drones_label = tk.Label(self, text="Available Drones:")
-        self.drones_label.grid(row=0, column=0)
-        self.drones_list = tk.Listbox(self)
-        self.drones_list.grid(row=1, column=0)
-        self.update_button = tk.Button(self, text="Update", command=self.update)
-        self.update_button.grid(row=2, column=0)
+        self.drones_list = tk.Listbox(self.root)
+        self.drones_list.grid(row=0, column=0, columnspan=2)
+        self.drones_list.insert(tk.END, "Available drones:")
+        self.update()
 
-        # show drone_id and shipment
-        self.drone_id_label = tk.Label(self, text="Drone ID:")
-        self.drone_id_label.grid(row=0, column=1)
-        self.drone_id_entry = tk.Entry(self)
+        # ask for drone_id and shipment address
+        self.drone_id_label = tk.Label(self.root, text="Drone ID:")
+        self.drone_id_label.grid(row=1, column=0)
+        self.drone_id_entry = tk.Entry(self.root)
         self.drone_id_entry.grid(row=1, column=1)
-        self.shipment_label = tk.Label(self, text="Shipment:")
-        self.shipment_label.grid(row=0, column=2)
-        self.shipment_entry = tk.Entry(self)
-        self.shipment_entry.grid(row=1, column=2)
-        self.submit_button = tk.Button(self, text="Submit", command=self.submit)
-        self.submit_button.grid(row=2, column=2)
+        self.shipment_label = tk.Label(self.root, text="Shipment address:")
+        self.shipment_label.grid(row=2, column=0)
+        self.shipment_entry = tk.Entry(self.root)
+        self.shipment_entry.grid(row=2, column=1)
+        self.submit_button = tk.Button(self.root, text="Submit", command=self.submit)
+        self.submit_button.grid(row=3, column=0, columnspan=2)
+        self.update_button = tk.Button(self.root, text="Update", command=self.update)
+        self.update_button.grid(row=4, column=0, columnspan=2)
+        self.quit_button = tk.Button(self.root, text="Quit", command=self.root.destroy)
+        self.quit_button.grid(row=5, column=0, columnspan=2)
 
-        # update button
-        self.update_button = tk.Button(self, text="Update", command=self.update)
-        self.update_button.grid(row=2, column=0)
+
 
     def update(self):
         self.client.request_update()
@@ -58,7 +65,12 @@ class Client_GUI(tk.Frame):
         self.drone_id_entry.delete(0, tk.END)
         self.shipment_entry.delete(0, tk.END)
 
-    
+    def task(self) -> None:
+        """
+        Main task of the client.
+        """
+        self.client.main_tick()
+        self.root.after(self.__time_after, self.task)
 
     
 
