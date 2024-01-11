69713506cbeae885ba2e99465f39344df22f8c77
Norbert Gabos <dont.vip@gmail.com>
2024-01-11 15:02:29 +0100

    fixed case where the pizza was in an order

--- Second-year/Basi di Dati/Pizzeria/scripts/remove_pizza.php
+++ Second-year/Basi di Dati/Pizzeria/scripts/remove_pizza.php
@@ -1,37 +1,47 @@
 <?php
 $data = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/setup.json'), true);
 
-// Connect to the database
 $conn = new mysqli("localhost", $data['dbUserName'], $data['dbPassword'], $data['dbName']);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get the pizza name from the GET request
 $pizzaName = $_GET['nome'];
 
-// Delete all ingredients from composizione for the given pizza
-$stmt = $conn->prepare("DELETE FROM composizione WHERE nomePizza = ?");
+$stmt = $conn->prepare("SELECT *
+                        FROM comprende
+                        WHERE nomePizza = ?");
+
 $stmt->bind_param("s", $pizzaName);
 $stmt->execute();
+$result = $stmt->get_result();
 
-echo "Deleted all ingredients from composizione for the given pizza";
+if ($result->num_rows > 0) {
+    $stmt->close();
+    $conn->close();
 
-// Prepare and bind
-$stmt = $conn->prepare("DELETE FROM pizze WHERE nome = ?");
-$stmt->bind_param("s", $pizzaName);
+    echo "<script>
+            alert('Non puoi eliminare una pizza che è presente in un ordine.');
+            window.location.href='/pizze.php';
+          </script>"; 
+} else {
+    $stmt = $conn->prepare("DELETE FROM composizione WHERE nomePizza = ?");
+    $stmt->bind_param("s", $pizzaName);
+    $stmt->execute();
 
-// Execute the statement
-$stmt->execute();
+    echo "Deleted all ingredients from composizione for the given pizza";
 
-echo "Deleted the pizza";
+    $stmt = $conn->prepare("DELETE FROM pizze WHERE nome = ?");
+    $stmt->bind_param("s", $pizzaName);
 
-// Close the database connection
-$stmt->close();
-$conn->close();
+    $stmt->execute();
 
-// Redirect back to the show pizzas page (replace with your actual page)
-header("Location: /pizze.php");
+    echo "Deleted the pizza";
+
+    $stmt->close();
+    $conn->close();
+
+    header("Location: /pizze.php");
+}
 ?>
