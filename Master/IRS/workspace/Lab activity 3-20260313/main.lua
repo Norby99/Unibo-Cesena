0379f49d646a664351e6b787a184b0ced6990361
Norby99 <gabos.norbert99@gmail.com>
2026-03-21 15:43:29 +0100

    doc(IRS): lab3 add doc

--- Master/IRS/workspace/Lab activity 3-20260313/main.lua
+++ Master/IRS/workspace/Lab activity 3-20260313/main.lua
@@ -1,15 +1,26 @@
 -- Put your global variables here
-MAX_VELOCITY = 15
-MAX_LIGHT_PERCIVED = 0.3
+MAX_VELOCITY = 15	-- The maximum velocity of the robot, in pixels per second
+
+--[[ The maximum values perceived by the robot's sensors, used for normalizing the
+sensor values. These values can be adjusted based on the specific characteristics of
+the robot's sensors and the environment it operates in.
+]]
+MAX_LIGHT_PERCIVED = 0.3	
 MAX_PROXIMITY_PERCIVED = 1.0
 MIN_MOTOR_GROUND_PERCIVED = 0.01
 
+--[[ The activation thresholds for the behaviors. These values determine when a
+behavior should be activated based on the perceived sensor values. Adjusting these
+thresholds can help fine-tune the robot's behavior in different environments.
+]]
 FEAR_ACTIVATION_THRESHOLD = 0.03
 FOLLOW_ACTIVATION_THRESHOLD = 0.02
 
-ROTATION_FACTOR = 40.0
-MIN_RELATIVE_DIFF = 0.05
-FEAR_BLIND_SPOT_PERCENTAGE = 0.23
+ROTATION_FACTOR_FEAR = 40.0		-- This factor determines how sharply the robot will turn when the fear behavior is activated.
+ROTATION_FACTOR_FOLLOW = 20.0	-- This factor determines how sharply the robot will turn when the follow behavior is activated.
+
+MIN_RELATIVE_DIFF = 0.05	-- The minimum relative difference between the left and right sensor groups for the behaviors to activate.
+FEAR_BLIND_SPOT_PERCENTAGE = 0.23	-- The percentage of the front sensors that are considered a blind spot for the fear behavior.
 
 -- Imports
 local SubsumptionController = require("libraries.subsumption_controller")
@@ -28,8 +39,8 @@ function init()
 
 	controller = SubsumptionController.new(MAX_VELOCITY)
 	controller:add(StopAtDarkSpot.new(MAX_VELOCITY, robot.motor_ground, MIN_MOTOR_GROUND_PERCIVED))
-	controller:add(Fear.new(MAX_VELOCITY, robot.proximity, MAX_PROXIMITY_PERCIVED, FEAR_ACTIVATION_THRESHOLD, ROTATION_FACTOR, MIN_RELATIVE_DIFF, FEAR_BLIND_SPOT_PERCENTAGE))
-	controller:add(Follow.new(MAX_VELOCITY, robot.light, MAX_LIGHT_PERCIVED, FOLLOW_ACTIVATION_THRESHOLD, ROTATION_FACTOR, MIN_RELATIVE_DIFF))
+	controller:add(Fear.new(MAX_VELOCITY, robot.proximity, MAX_PROXIMITY_PERCIVED, FEAR_ACTIVATION_THRESHOLD, ROTATION_FACTOR_FEAR, MIN_RELATIVE_DIFF, FEAR_BLIND_SPOT_PERCENTAGE))
+	controller:add(Follow.new(MAX_VELOCITY, robot.light, MAX_LIGHT_PERCIVED, FOLLOW_ACTIVATION_THRESHOLD, ROTATION_FACTOR_FOLLOW, MIN_RELATIVE_DIFF))
 end
 
 function step()
